e24c62989c290d898c28a15cc1e1334198db7567
keith.shum <keith.shum@oracle.com>
2019-10-30 11:21:31 -0700

    Change2 adding more layers

--- examples/keras-perceptron/perceptron.py
+++ examples/keras-perceptron/perceptron.py
@@ -24,9 +24,16 @@ labels = [str(i) for i in range(10)]
 
 num_classes = y_train.shape[1]
 
+### Change2
+### Adding more layers
 # create model
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(img_width, img_height)))
+model.add(tf.keras.layers.Dense(50, activation='relu'))
+model.add(tf.keras.layers.Dense(100, activation='relu'))
+model.add(tf.keras.layers.Dense(200, activation='relu'))
+model.add(tf.keras.layers.Dense(400, activation='relu'))
+model.add(tf.keras.layers.Dense(50, activation='relu'))
 model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam',
               metrics=['accuracy'])
